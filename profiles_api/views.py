916e76faa9543bd474105572301f7222056292bd
Gokul <gokul.r@kodehash.com>
2022-11-14 16:59:25 +0530

    added filters

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import HelloSerializer
-from rest_framework import status,viewsets
+from rest_framework import status,viewsets,filters
 from profiles_api import serializers
 from profiles_api import models
 from rest_framework.authentication import TokenAuthentication
@@ -15,6 +15,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
 
 
 class HelloApiView(APIView):
